6ee1f09b7e4c995b845d20fbc08640349ac3d620
sergdt <94560886+sergdt@users.noreply.github.com>
2022-08-25 18:33:16 -0500

    Added Spring dependencies

---
+++ src/main/java/com/vuelafacil/VuelafacilApplication.java
@@ -0,0 +1,13 @@
+package com.vuelafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuelafacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vuelafacil/VuelafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuelafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
