8f0bb0a419721f70082a4a41cc2e38152503e53d
IvanBM03 <111829717+IvanBM03@users.noreply.github.com>
2022-09-08 21:17:58 -0500

    create Ivan java

---
+++ src/main/java/com/vuelafacil/entidades/Ivan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vuelafacil.entidades;
+
+/**
+ *
+ * @author Ivan Javier
+ */
+public class Ivan {
+    
+    private String nombre;
+    
+    
+}
